ab7ba7dadfeeecb150b2f2dac977056e6be9b9f7
Yuha0123 <44527599+Yuha0123@users.noreply.github.com>
2021-07-09 13:25:41 +0900

    Create 11050.cpp

---
+++ BOJ/11050.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#define MAX 10
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	int dp[MAX + 1][MAX + 1] = { 0, };
+
+	cin >> n >> k;
+
+	//init
+	for (int i = 0; i <= n; i++)
+	{
+		dp[i][0] = 1;
+		dp[i][i] = 1;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			dp[i + 1][j + 1] = (dp[i][j] + dp[i][j + 1]);
+		}
+	}
+
+	cout << dp[n][k];
+}
